e2741f78e573300c33d5d7c8333c5e41ad6a7ded
fernandakaory <fernandakaory2@gmail.com>
2025-03-10 21:57:36 -0300

    adicionando uma dependencia

--- src/main/java/com/github/fernandakaory/study_apix/ControllerPing.java
+++ src/main/java/com/github/fernandakaory/study_apix/ControllerPing.java
@@ -1,5 +1,5 @@
 package com.github.fernandakaory.study_apix;
-
+//mvn spring-boot:run
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
