f3759abc6328536ce77c5ef59ad037cdeca74afc
fernandakaory <fernandakaory2@gmail.com>
2025-03-15 20:06:47 -0300

    Add resquest body

--- src/main/java/com/github/fernandakaory/study_apix/ControllerProduto.java
+++ src/main/java/com/github/fernandakaory/study_apix/ControllerProduto.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 public class ControllerProduto {
+
     @PostMapping
-    public ResponseEntity<String> create(Produto produto) {
-        return ResponseEntity.status(201).body("Produto Cadastrado");
+    public ResponseEntity<String> create(@RequestBody Produto produto) {
+        return ResponseEntity.status(201).body("Produto cadastro");
     }
 
     @PutMapping
