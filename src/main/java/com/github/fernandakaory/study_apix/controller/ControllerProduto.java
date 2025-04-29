173a672ee765d191ca423dcb23590a4a52d2595c
fernandakaory <fernandakaory2@gmail.com>
2025-04-28 22:46:43 -0300

    aula2804

--- src/main/java/com/github/fernandakaory/study_apix/controller/ControllerProduto.java
+++ src/main/java/com/github/fernandakaory/study_apix/controller/ControllerProduto.java
@@ -1,8 +1,11 @@
 package com.github.fernandakaory.study_apix.controller;
+import java.math.BigDecimal;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -31,39 +34,50 @@ public class ControllerProduto {
     private ProdutoService produtoService = new ProdutoService();
 
     @PostMapping
-    public ResponseEntity<Produto> create(@RequestBody ProdutoRequestCreate dto) {
-
-        Produto produto = produtoService.save(dto);
-
-        return ResponseEntity.status(201).body(produto);
+    public ResponseEntity<ProdutoResponse>
+                create(@RequestBody ProdutoRequestCreate dto) {
+        return ResponseEntity.status(201).body(
+            new ProdutoResponse().toDto(produtoService.save(dto))
+        );
     }
+ 
 
     @PutMapping("{id}")
     public ResponseEntity<ProdutoResponse> update(
             @PathVariable Long id,
             @RequestBody ProdutoRequestUpdate dto) {
+
         return produtoService.update(id, dto)
-            .map(produto -> {
-                ProdutoResponse response = new ProdutoResponse();
-                response.setId(produto.getId());
-                response.setNome(produto.getNome());
-                return response;
-    
-        })
-                .map(ResponseEntity::ok)
+            .map(produto -> ResponseEntity.ok(new ProdutoResponse().toDto(produto)))
                 .orElse(ResponseEntity.notFound().build());
     }
     
     @GetMapping
-    public ResponseEntity<List<Produto>> findAll() {
-        List<Produto> produtos = produtoService.findAll();
-        return ResponseEntity.status(200).body(produtos);
+    public ResponseEntity<List<ProdutoResponse>> findAll() {
+
+        return ResponseEntity.ok(produtoService
+            .findAll()
+            .stream()
+
+            .map(produto -> {ProdutoResponse response = new ProdutoResponse();
+            response.setId(produto.getId());
+            response.setNome(produto.getNome());
+            return response;
+        })
+            .collect(Collectors.toList()));
+        
     }
 
     @GetMapping("{id}")
-    public ResponseEntity <Produto> findById(@PathVariable Long id) {
+    public ResponseEntity<ProdutoResponse> findById(@PathVariable Long id) {
        return produtoService.findById(id)
-            .map(p -> ResponseEntity.ok(p))
+            .map(produto -> {
+                ProdutoResponse response = new ProdutoResponse();
+                response.setId(produto.getId());
+                response.setNome(produto.getNome());
+                return ResponseEntity.ok(response);
+    
+        })
             .orElse(ResponseEntity.notFound().build());
     }
 
--- src/main/java/com/github/fernandakaory/study_apix/dto/ProdutoResponse.java
+++ src/main/java/com/github/fernandakaory/study_apix/dto/ProdutoResponse.java
@@ -1,8 +1,16 @@
 package com.github.fernandakaory.study_apix.dto;
 
+import com.github.fernandakaory.study_apix.model.Produto;
+
 public class ProdutoResponse {
     private Long id;
     private String nome;
+
+    public ProdutoResponse toDto (Produto produto) {
+        this.setId(produto.getId());
+        this.setNome(produto.getNome());
+        return this;
+    }
     
     public Long getId() {
         return id;
